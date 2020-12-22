8675f53b5c5bde5eb7d8968be0f16da72a786f8c
romgrm <romaingreaume@gmail.com>
2020-12-22 12:28:59 +0100

    REINSTALL BDD SQL

---
+++ .history/src/App_20201222102001.js
@@ -0,0 +1,24 @@
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Header from './Components/Header'
+import {Switch, Route} from 'react-router-dom'
+import Films from './Components/Films'
+import Events from './Components/Events';
+import Village from './Components/Village';
+import Home from './Components/Home';
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Switch>
+      <Route path="/Events" component={Events} />
+      <Route path="/Movies" component={Films} />
+      <Route path="/Places" component={Village} />
+      <Route path="/" component={Home}/>
+    </Switch>
+    </>
+  );
+}
+
+export default App;
